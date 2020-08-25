b09f5e80a80fd04d46b447afccfd446d0a7d32ec
filippo-fonseca <filifonsecacagnazzo@gmail.com>
2020-08-25 07:32:20 -0600

    Initial commit

---
+++ lang.ts
@@ -0,0 +1,108 @@
+var fs = require("fs");
+
+let returnIt = "";
+
+let setLimit = 20;
+
+for (let i = 0; i < setLimit; i++) {
+  // Verbs
+  var randomVerb = function (words) {
+    let readME = fs.readFileSync("verbs.txt", "utf8");
+    let wordArr = readME.split(", ");
+
+    words = words > wordArr.length ? wordArr.length : words;
+
+    var randWords = [];
+    for (let i = 0; i < words; i++) {
+      let newRandom;
+      do {
+        let rand = Math.floor(Math.random() * wordArr.length);
+        newRandom = wordArr[rand];
+      } while (randWords.includes(newRandom));
+
+      randWords.push(newRandom);
+    }
+    return randWords.join(", ");
+  };
+  // Nouns
+  var randomNoun = function (words) {
+    let readME = fs.readFileSync("nouns.txt", "utf8");
+    let wordArr = readME.split(", ");
+
+    words = words > wordArr.length ? wordArr.length : words;
+
+    var randWords = [];
+    for (let i = 0; i < words; i++) {
+      let newRandom;
+      do {
+        let rand = Math.floor(Math.random() * wordArr.length);
+        newRandom = wordArr[rand];
+      } while (randWords.includes(newRandom));
+
+      randWords.push(newRandom);
+    }
+    return randWords.join(", ");
+  };
+
+  // Articles
+  var randomArt = function (words) {
+    let readME = fs.readFileSync("articles.txt", "utf8");
+    let wordArr = readME.split(", ");
+
+    words = words > wordArr.length ? wordArr.length : words;
+
+    var randWords = [];
+    for (let i = 0; i < words; i++) {
+      let newRandom;
+      do {
+        let rand = Math.floor(Math.random() * wordArr.length);
+        newRandom = wordArr[rand];
+      } while (randWords.includes(newRandom));
+
+      randWords.push(newRandom);
+    }
+    return randWords.join(", ");
+  };
+
+  // Adjectives
+  var randomAdj = function (words) {
+    let readME = fs.readFileSync("adjectives.txt", "utf8");
+    let wordArr = readME.split(", ");
+
+    words = words > wordArr.length ? wordArr.length : words;
+
+    var randWords = [];
+    for (let i = 0; i < words; i++) {
+      let newRandom;
+      do {
+        let rand = Math.floor(Math.random() * wordArr.length);
+        newRandom = wordArr[rand];
+      } while (randWords.includes(newRandom));
+
+      randWords.push(newRandom);
+    }
+    return randWords.join(", ");
+  };
+
+  function combineIt() {
+    let nounPhrase =
+      randomArt(1) + " " + randomAdj(1) + " " + randomNoun(1) + " ";
+    let verbPhrase =
+      randomVerb(1) +
+      " " +
+      randomArt(1) +
+      " " +
+      randomAdj(1) +
+      " " +
+      randomNoun(1) +
+      ". ";
+
+    return nounPhrase + verbPhrase;
+  }
+
+  returnIt = returnIt + combineIt();
+}
+
+returnIt = returnIt + ".";
+
+console.log(returnIt);
